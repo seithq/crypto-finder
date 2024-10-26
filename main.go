e7c7f6b8638412eda50df6d6c0a227493604c5ce
Daniyar Baitursynov <daniyar@hey.com>
2024-10-26 20:52:15 +0500

    Push new changes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/ecdsa"
@@ -16,6 +17,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -25,45 +27,85 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-const INFURA = "https://mainnet.infura.io/v3/8f55e3b466dc48da85e06b50177c4c0b"
+const (
+	INFURA       = "https://mainnet.infura.io/v3/8f55e3b466dc48da85e06b50177c4c0b"
+	hexChars     = "0123456789abcdef"
+	workersCount = 10 // количество воркеров
+)
+
+var (
+	baseString = "0a5c2dffb9a6e1240e7d8f58b1e68d6c9fce1e6e9b0a5c0e7f1b2c3a4b5"
+)
+
+// Генерация всех возможных комбинаций из 5 символов
+func generateCombinations(prefix string, length int, result chan<- string, wg *sync.WaitGroup, sem chan struct{}) {
+	defer wg.Done()
+	if length == 0 {
+		result <- prefix
+		return
+	}
+	for _, c := range hexChars {
+		sem <- struct{}{}
+		wg.Add(1)
+		go generateCombinations(prefix+string(c), length-1, result, wg, sem)
+		<-sem
+	}
+}
 
 func main() {
-	// client, err := ethclient.Dial(INFURA)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer client.Close()
+	// combinations := make(chan string)
+	// var wg sync.WaitGroup
+	// sem := make(chan struct{}, workersCount)
+
+	// // Запуск воркера для обработки комбинаций
+	// go func() {
+	// 	for combo := range combinations {
+	// 		for i := 0; i <= len(baseString); i++ {
+	// 			// Добавляем недостающие символы перед строкой
+	// 			if i == 0 {
+	// 				priv := combo + baseString
+	// 				fmt.Println(strings.ToLower(priv + "=" + addressFromPrivate(priv)))
+	// 			}
+	// 			// Добавляем недостающие символы после строки
+	// 			if i == len(baseString) {
+	// 				priv := baseString + combo
+	// 				fmt.Println(strings.ToLower(priv + "=" + addressFromPrivate(priv)))
+	// 			}
+	// 		}
+	// 	}
+	// }()
+
+	// // Инициализация генерации комбинаций
+	// wg.Add(1)
+	// go generateCombinations("", 5, combinations, &wg, sem)
+
+	// // Ожидаем завершения всех горутин
+	// wg.Wait()
+	// close(combinations)
 
 	wrk := func(_ int, jobs <-chan string, results chan<- string) {
-		for hash := range jobs {
-
-			hexBuf, _ := hex.DecodeString(hash)
-			priv := ToECDSA(hexBuf)
-			pub := priv.PublicKey
-
-			ecdsaPubBytes := elliptic.Marshal(secp256k1.S256(), pub.X, pub.Y)
-			addressBytes := Keccak256(ecdsaPubBytes[1:])[12:]
-
-			address := fmt.Sprintf("0x%s", hex.EncodeToString(addressBytes))
-			// result := fmt.Sprintf("%s -> %s -> %s", hash, address, getBalance(client, address))
-			result := fmt.Sprintf("%s -> %s -> %s", hash, address, getTokens(address))
-
+		for address := range jobs {
+			result := fmt.Sprintf("%s=%s", address, getTokens([]string{address}))
 			results <- result
 		}
 	}
 
-	hashes := parseHashes("storage/input.txt")
-	len := len(hashes)
+	full := parseCombinations("storage/combinations.txt")
+
+	start := 0
+	len := 100000
+
+	addresses := full[start : start+len]
 
 	jobs := make(chan string, len)
 	results := make(chan string, len)
 
-	for w := 1; w <= 3; w++ {
+	for w := 1; w <= 10; w++ {
 		go wrk(w, jobs, results)
 	}
 
 	for j := 0; j < len; j++ {
-		jobs <- hashes[j]
+		jobs <- addresses[j]
 	}
 	close(jobs)
 
@@ -72,8 +114,8 @@ func main() {
 	}
 }
 
-func parseHashes(filename string) []string {
-	hashes := make([]string, 0)
+func parseCombinations(filename string) []string {
+	addresses := make([]string, 0)
 
 	file, err := os.Open(filename)
 	if err != nil {
@@ -86,20 +128,16 @@ func parseHashes(filename string) []string {
 
 	for scanner.Scan() {
 		text := scanner.Text()
-		if strings.HasPrefix(text, "---") {
-			continue
-		}
 
-		parts := strings.Split(text, ":")
+		parts := strings.Split(text, "=")
 		if len(parts) < 2 {
 			continue
 		}
 
-		hash := strings.Trim(parts[1], " ")
-		hashes = append(hashes, hash)
+		addresses = append(addresses, strings.Trim(parts[1], " "))
 	}
 
-	return hashes
+	return addresses
 }
 
 func getBalance(client *ethclient.Client, rawAddress string) string {
@@ -122,20 +160,76 @@ func getBalance(client *ethclient.Client, rawAddress string) string {
 	return ethValue.String()
 }
 
-type Token struct {
-	Symbol string `json:"symbol"`
+type MethodResponse struct {
+	ID      int    `json:"id"`
+	JsonRPC string `json:"jsonrpc"`
+	Result  struct {
+		Address       string  `json:"address"`
+		TokenBalances []Token `json:"tokenBalances"`
+	} `json:"result"`
 }
 
-func getTokens(rawAddress string) string {
-	url := fmt.Sprintf("https://deep-index.moralis.io/api/v2.2/%s/erc20?chain=eth", rawAddress)
+type Token struct {
+	ContractAddress string `json:"contractAddress"`
+	TokenBalance    string `json:"tokenBalance"`
+}
 
-	req, err := http.NewRequest("GET", url, nil)
+type Param struct {
+	ID      int      `json:"id"`
+	JsonRPC string   `json:"jsonrpc"`
+	Method  string   `json:"method"`
+	Params  []string `json:"params"`
+}
+
+func parseTokens(tokens []Token) string {
+	result := make([]string, 0)
+
+	symbolsMap := map[string]string{
+		"0x6b175474e89094c44da98b954eedeac495271d0f": "DAI",
+		"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": "WETH",
+		"0xdAC17F958D2ee523a2206206994597C13D831ec7": "USDT",
+	}
+
+	emptyBalance := "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+	for _, token := range tokens {
+		symbol, ok := symbolsMap[token.ContractAddress]
+		if ok && token.TokenBalance != emptyBalance {
+			result = append(result, symbol+"#"+token.TokenBalance)
+		}
+	}
+
+	if len(result) == 0 {
+		return "ZER"
+	}
+
+	return strings.Join(result, ",")
+}
+
+func getTokens(addresses []string) string {
+	url := "https://eth-mainnet.g.alchemy.com/v2/alcht_nqnlLRumvopEQ4nLZDQWNyBz41URJW"
+
+	addrs := make([]string, len(addresses))
+	for i := 0; i < len(addresses); i++ {
+		addrs[i] = "0x" + addresses[i]
+	}
+
+	params := Param{
+		ID:      1,
+		JsonRPC: "2.0",
+		Method:  "alchemy_getTokenBalances",
+		Params:  addrs,
+	}
+
+	payload, err := json.Marshal(&params)
 	if err != nil {
 		return err.Error()
 	}
 
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("X-API-Key", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjgyOGQ3NjhhLTE2MzMtNDRjNC05NmY1LTFjNmZjZWQxNDM4ZiIsIm9yZ0lkIjoiNDEzMjE2IiwidXNlcklkIjoiNDI0NjU0IiwidHlwZUlkIjoiZGI4Y2FlNjktMDNhOC00YjAxLTgyNzUtZTU2OTAwOTY2M2ZlIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3Mjk4MDYwODgsImV4cCI6NDg4NTU2NjA4OH0.GFcjwGsXqpDo6KLG-tHhmfajS6rMNR7a_-2ULa51vGU")
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
+	if err != nil {
+		return err.Error()
+	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -147,23 +241,19 @@ func getTokens(rawAddress string) string {
 	if err != nil {
 		return err.Error()
 	}
+	defer res.Body.Close()
 
-	tokens := []Token{}
-	err = json.Unmarshal(body, &tokens)
+	methodResponse := &MethodResponse{}
+	err = json.Unmarshal(body, &methodResponse)
 	if err != nil {
 		return err.Error()
 	}
 
-	if len(tokens) == 0 {
-		return "NIL"
+	if len(methodResponse.Result.TokenBalances) == 0 {
+		return "ZER"
 	}
 
-	out := []string{}
-	for _, token := range tokens {
-		out = append(out, token.Symbol)
-	}
-
-	return strings.Join(out, ",")
+	return parseTokens(methodResponse.Result.TokenBalances)
 }
 
 func BytesToBig(data []byte) *big.Int {
@@ -213,4 +303,14 @@ func decryptAES(encryptedString, keyString, nonceString string) (string, error) 
 	}
 
 	return string(plaintext), nil
+}
+
+func addressFromPrivate(privateKey string) string {
+	hexBuf, _ := hex.DecodeString(privateKey)
+	priv := ToECDSA(hexBuf)
+	pub := priv.PublicKey
+
+	ecdsaPubBytes := elliptic.Marshal(secp256k1.S256(), pub.X, pub.Y)
+	addressBytes := Keccak256(ecdsaPubBytes[1:])[12:]
+	return hex.EncodeToString(addressBytes)
 }
